f735e5c72139d40945cfcb1575fe6978aba202aa
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-04-06 00:55:58 -0700

    adding all the views

--- src/js/views/summaryView.js
+++ src/js/views/summaryView.js
@@ -1,5 +1,34 @@
-import View from "./View.js";
+import { formatCurrency, formatMovementDate } from "../helpers.js";
 
-class UserView extends View {
-  _parentElement = document.querySelector(".");
+class SummaryView {
+  _parentElement = document.querySelector(".summary");
+
+  generateMarkup(acc) {
+    // 1) entries
+    const incomes = acc.movements
+      .filter((mov) => mov > 0)
+      .reduce((acc, mov) => acc + mov, 0);
+    this._parentElement.querySelector(".summary__value--in").textContent =
+      formatCurrency(incomes, acc.locale, acc.currency);
+
+    // 2) exits
+    const out = acc.movements
+      .filter((mov) => mov < 0)
+      .reduce((acc, mov) => acc + mov, 0);
+    this._parentElement.querySelector(".summary__value--out").textContent =
+      formatCurrency(Math.abs(out), acc.locale, acc.currency);
+
+    // 3) interests
+    const interest = acc.movements
+      .filter((mov) => mov > 0)
+      .map((deposit) => (deposit * acc.interestRate) / 100)
+      .filter((int) => {
+        return int >= 1;
+      })
+      .reduce((acc, int) => acc + int, 0);
+    this._parentElement.querySelector(".summary__value--interest").textContent =
+      formatCurrency(interest, acc.locale, acc.currency);
+  }
 }
+
+export default new SummaryView();
